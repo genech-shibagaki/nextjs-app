4e878323be14a85dc967a87519addd8893fe74a7
Shibagaki Masato <shibagaki@genech.local>
2025-06-24 14:50:28 +0900

    Aboutページを更新（自動デプロイテスト）

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
               className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
-              width={20}
+              width={200}
               height={20}
             />
             Deploy now
